11cc769f835644c66dff1e0b78e377ea4169c3a5
G7M7T1 <genlisense@gmail.com>
2021-10-02 14:20:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# 🚀 Svelte App
+
+这是 [Svelte](https://svelte.dev) 应用程序的项目模板。 它位于 https://github.com/sveltejs/template
+
+使用此模板创建一个新项目 [degit](https://github.com/Rich-Harris/degit):
+
+```bash
+npx degit sveltejs/template svelte-app
+cd svelte-app
+```
+
+_请注意，您需要安装 [Node.js](https://nodejs.org)_
+
+## 开始
+
+安装依赖项...
+
+```bash
+cd svelte-app
+npm install
+```
+
+...然后开始 [Rollup](https://rollupjs.org)：
+
+```bash
+npm run dev
+```
+
+导航到 [localhost:5000](http://localhost:5000) 你应该会看到你的应用程序正在运行 \
+在 `src` 中编辑一个组件文件，保存它，然后重新加载页面以查看你的更改
+
+默认情况下，服务器只会响应来自本地主机的请求 要允许来自其他计算机的连接，请编辑 package.json 中的 `sirv` 命令以包含选项 `--host 0.0.0.0`
+
+如果你使用 [Visual Studio Code](https://code.visualstudio.com/)，我们建议安装官方扩展 [Svelte for VS Code](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode） 如果你使用其他编辑器，你可能需要安装插件才能获得语法高亮
---
+++ src/App.svelte
@@ -0,0 +1,131 @@
+<script>
+  let d = 0;
+  let h = 0;
+  let m = 0;
+  let s = 0;
+
+  const newYearTime = new Date(`Jan 01 2022 00:00:00 GMT-0400`);
+  function updateCountdown() {
+    const currentTime = new Date();
+    const diff = newYearTime - currentTime;
+    d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    d = d < 10 ? "0" + d : d;
+    h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    h = h < 10 ? "0" + h : h;
+    m = Math.floor(diff / 1000 / 60) % 60;
+    m = m < 10 ? "0" + m : m;
+    s = Math.floor(diff / 1000) % 60;
+    s = s < 10 ? "0" + s : s;
+  }
+
+  setInterval(updateCountdown, 1000);
+</script>
+
+<main>
+  <div class="year" id="year" />
+  <h1>New Year Coming</h1>
+  <div class="countdown" id="countdown">
+    <div class="time">
+      <h2 id="days">{d}</h2>
+      <small>Days</small>
+    </div>
+
+    <div class="time">
+      <h2 id="hours">{h}</h2>
+      <small>Hours</small>
+    </div>
+
+    <div class="time">
+      <h2 id="minutes">{m}</h2>
+      <small>Minutes</small>
+    </div>
+
+    <div class="time">
+      <h2 id="seconds">{s}</h2>
+      <small>Seconds</small>
+    </div>
+  </div>
+
+  <!-- <img src="/loading.gif" alt="Loading Info" class="loading" id="loading" /> -->
+</main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Gemunu+Libre&display=swap");
+  * {
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    background-image: url("photo1.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center center;
+    height: 100vh;
+    color: white;
+    font-family: "Gemunu Libre", sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin: 0;
+    overflow: hidden;
+  }
+
+  :global(body)::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.4);
+  }
+
+  :global(body) * {
+    z-index: 1;
+  }
+
+  h1 {
+    font-size: 4rem;
+    margin: -80px 0 40px;
+  }
+
+  .countdown {
+    display: flex;
+    transform: scale(2);
+    align-items: center;
+    justify-content: center;
+  }
+
+  .time {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 15px;
+  }
+
+  .time h2 {
+    margin: 0 0 5px;
+  }
+
+  @media (max-width: 500px) {
+    h1 {
+      font-size: 45px;
+    }
+
+    .time {
+      margin: 5px;
+    }
+
+    .time h2 {
+      font-size: 12px;
+      margin: 0;
+    }
+
+    .time small {
+      font-size: 10px;
+    }
+  }
+</style>
